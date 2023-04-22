f92e7ebb33e206dda9025b4e7c52877d3a9ba7c6
Dayvidh <122726923+Dayvidh@users.noreply.github.com>
2023-04-22 03:53:36 -0700

    Create itertools.permutations.py

---
+++ itertools.permutations.py
@@ -0,0 +1,8 @@
+from itertools import permutations
+def per(s, size):
+    return "\n".join(sorted(["".join(i) for i in permutations(s,size)]))
+user = input().split(" ")
+
+s, size = user[0], int(user[1])
+
+print(per(s,size))
